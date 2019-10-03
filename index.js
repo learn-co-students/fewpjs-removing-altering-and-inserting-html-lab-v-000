362180672566aa081fac89ec903778461dcdff87
Joeycho <joeyadamcho@gmail.com>
2019-10-03 07:24:40 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+let main = document.querySelector('main#main');
+
+main.remove();
+
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+
+newHeader.id = "victory";
+newHeader = document.querySelector('h1#victory');
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
