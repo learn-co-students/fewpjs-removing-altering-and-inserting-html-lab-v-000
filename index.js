b85fb602c58029e56349455928c59e0c2b945a07
dhintz <dhintz89@gmail.com>
2019-10-03 20:09:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById("main").remove()
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Dan is the champion";
+
+document.body.appendChild(newHeader)
