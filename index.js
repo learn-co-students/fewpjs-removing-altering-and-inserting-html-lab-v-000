62b5f0cf4e0cd2e1967c7c0e8c23711341e2e59a
Sushi Chanrai <sushi@pathworks.co>
2019-10-07 15:28:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+
+let main = document.getElementById('main');
+main.remove();
+
+
+
+let newHeader = document.createElement('h1');
+newHeader.id = ("victory");
+//you don't need to append - already being added to the DOM with command above
+//document.body.appendChild(head);
+let myName = "Sushi Chanrai"
+newHeader.innerHTML = `${myName} is the champion`
