4777d1f93d5212ca0c19743662b7fc9d54934b8c
Nick-Damico <=>
2019-10-07 21:42:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+// Test 1
+document.querySelector('#main').remove()
+
+// Test 2
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+
+// Test 3
+newHeader.textContent = 'HTML is the champion'
