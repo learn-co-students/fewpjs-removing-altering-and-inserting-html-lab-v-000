70c4e18b7b4ac07d59b81e099910bf75aff7971e
Richard Cho <richardcho100@gmail.com>
2019-10-13 22:52:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const main = document.querySelector('main');
+main.remove()
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+
+newHeader.setAttribute('id', 'victory')
+
+newHeader.innerHTML = "Richard is the champion"
