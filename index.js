49ce8154db47b5d175ae8c644ccaefbf8aa24e64
Meg Gutshall <meghan.gutshall@gmail.com>
2019-10-28 18:22:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+document.getElementById("main").remove();
+
+let newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+let paragraph = document.createElement("p");
+let text = document.createTextNode("Meg is the champion!");
+paragraph.appendChild(text);
+
+newHeader.appendChild(paragraph);
