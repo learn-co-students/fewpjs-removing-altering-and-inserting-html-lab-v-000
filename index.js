fdceb67c40dc0fefcbabe182e2b16284aba3253d
github username <github email address>
2019-11-03 12:51:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const toRemove = document.querySelector("main#main");
+toRemove.remove();
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion"
