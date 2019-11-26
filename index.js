a564e68c271a8e74c4776c75a7c362a4181e923d
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-11-25 22:05:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your code here!
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.className = 'victory';
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'Jourdan is the champion';
+
+
+// let element = document.createElement('div')
+//     document.body.appendChild(element)
+//     let ul = document.createElement('ul')
+ 
+//     for (let i = 0; i < 3; i++) {
+//       let li = document.createElement('li')
+//       li.innerHTML = (i + 1).toString()
+//       ul.appendChild(li)
+//     }
+
+
+//     let header = document.getElementById("div#header");
+// header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+
+// <div id="header">
+//   <h1>Poodles!</h1>
+//   <h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3>
+//   <p><em>By: Byron Q. Poodle, Esq., BA.</em></p>
+// </div>
+     
+//     element.style.backgroundColor = '#27647B';
+
+//     element.appendChild(ul)
+
+//     element.style.textAlign = 'center';
+// ul.style.textAlign = 'left'
