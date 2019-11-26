06f2565f57586470b580ea458d30057558a91bcb
Gingertonic <bethmschofield@gmail.com>
2019-11-26 15:26:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector('#main').remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "One is the champion!"
