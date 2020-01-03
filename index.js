9235fad9974b82a6fbdd979fa259a74560a0b9e0
John Blank <veloblank@gmail.com>
2020-01-03 00:47:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.getElementsByTagName("main")[0].remove();
+let newHeader = document.createElement("h1");
+
+newHeader.innerHTML = "John is the champion!";
+newHeader.setAttribute("id", "victory");
+document.body.appendChild = newHeader;
