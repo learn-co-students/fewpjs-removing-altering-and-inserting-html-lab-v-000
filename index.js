c99bead5c7ce80d241f4d0dcb4dd5164375d273b
github username <github email address>
2020-03-19 12:32:57 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+main.remove()
+
+
+// document.body.appendChild(element)
+
+let newHeader = document.createElement("h1")
+// this is how you set attributes on an element (these are two ways, a helper and dot notation )
+// newHeader.setAttribute("id", "victory")
+newHeader.id = "victory"
+newHeader.innerHTML = "Jad is the champion"
