25618a311424385bee6f96b73374a725189091be
inception88 <bljones36@gmail.com>
2020-03-24 21:49:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+let element = document.getElementById('main');
+element.remove();
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Ben is the champion';
