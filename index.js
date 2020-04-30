3a4dc2d6f4833576f94d8bd4c4c13bea7be14c5a
Liz P <parisielizabeth@gmail.com>
2020-04-30 02:15:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+document.getElementById("main").remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+
+newHeader.innerHTML = 'Liz is the champion'
