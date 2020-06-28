ea78f59144c2dd12cedf2982dcf15a5b856f078f
Joshua Needham <joshuabneedham@gmail.com>
2020-06-28 03:17:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+// Write your code here
+main.remove();
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Joshua is the champion';
