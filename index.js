1c0fb514855258240b45fa0aa88145cfdc576ce9
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-07-01 21:53:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+let main = document.querySelector('main')
+main.remove();
+
+let newHeader = document.createElement('h1')
+// newHeader.id = 'victory'
+newHeader.setAttribute("id","victory")
+
+document.getElementById('h1#victory');
+newHeader.innerHTML = "Mushroom is the champion";
