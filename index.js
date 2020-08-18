6bacdabd740e00f2db29fd546dea62a046881d96
Eric Johnson <ericjohnson0216@icloud.com>
2020-08-17 22:32:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code here!
+main.remove();
+// let element = document.createElement('header')
+var newHeader = document.createElement('h1');
+newHeader.id = "victory"
+document.body.appendChild(newHeader);
+newHeader.textContent = "YOUR-NAME is the champion"
+    // h1.innerHTML = (i + 1).toString()
+    // header.appendChild(h1)
+
+
+
+// let header = document.getElementById("header#victory");
+// header.innerHTML = "<h1>victory</h1>";
+// let newHeader = document.querySelector("header#victory");
+// element.innerHTML = "<h1>victory</h1>";
+// document.querySelector("header").innerHTML = "<h1>victory</h1>";
+
+// let element = document.querySelector("header");
+// element.innerHTML = "<h1>victory</h1>";
+// let header = document.getElementById("header");
+// header.innerHTML = "<h1>victory</h1>";
+// let newHeader = document.getElementById("header");
+// header.innerHTML = "<h1>victory</h1>";
