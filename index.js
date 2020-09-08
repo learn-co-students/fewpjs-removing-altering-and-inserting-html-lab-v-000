4d7923f828fcf5d9e81b099d62de31bdb981b7db
klovae <elyse.klova@gmail.com>
2020-09-07 21:42:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+
+let newHeader = document.createElement('h1');
+
+newHeader.id = "victory";
+
+document.body.appendChild(newHeader);
+
+newHeader.innerHTML = 'Elyse is the champion'
