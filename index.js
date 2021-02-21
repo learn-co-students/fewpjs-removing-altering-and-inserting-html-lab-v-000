d1fad5cbef9757baa6344e69fa99dba82c1dbd93
Andrew <andrewcaniglia@gmail.com>
2021-02-21 01:03:02 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let ul = document.getElementById('main');
+ul.remove();
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
+newHeader.innerHTML = 'YOUR-NAME is the champion';
