68215469fd24c393b0c9980361aa8636b18c486e
Bryan Hooper <bthooper@gmail.com>
2021-03-25 10:35:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+//
+//
+const mainElement = document.querySelector("main");
+
+mainElement.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+
+newHeader.innerHTML = "Bryan is the champion!";
+
+place = document.querySelector("body");
+
+place.appendChild(newHeader);
