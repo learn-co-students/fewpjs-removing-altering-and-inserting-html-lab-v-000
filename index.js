086936251cd618bbe5af3a1185770f2895d04c91
Thomas <Thomas.c.viles.jr@gmail.com>
2021-05-27 13:08:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+let body = document.querySelector("body");
+body.removeChild(body.querySelector('#main'));
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "YOUR-NAME is the champion";
